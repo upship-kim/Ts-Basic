83ad52e4fba03d47a3feaffff3728a635f78ac28
upship-kim <tkdqook@gmail.com>
2021-06-07 19:43:18 +0900

    test Typescript

--- src/practice.ts
+++ src/practice.ts
@@ -1,2 +1,9 @@
-const message:string = 'hello world';
-console.log(message); 
+let count = 0; //숫자
+count += 1; 
+count = '문자열';   //에러 
+
+const message:string = 'hello world';  ///문자열
+
+const done:boolean = true; //불리언 값 
+
+const numbers:number[] = [1,2,3] 
